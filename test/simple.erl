66d06291bfc041df51b51098d8a670edc7e090d0
UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
2011-10-08 23:44:01 +0900

    add readme

--- test/simple.erl
+++ test/simple.erl
@@ -5,10 +5,7 @@
 
 aaa_test() ->
     {ok,Pid}=gen_event:start({local, ?MODULE}),
-    ?debugHere,
     ok = gen_event:add_handler(?MODULE, fluent_event, debug),
-    ?debugHere,
-    ?debugVal(Pid),
     ok = gen_event:notify(?MODULE, {debug, {[{<<"hoge">>,<<"data">>}]}}),
 %    error_logger:error_msg("hogehoge"),
 
